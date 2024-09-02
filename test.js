404e5ac73779b28797e34fb367c25800308d510e
WILLIAN GABRIEL LOPES DA SILVA <WILLIAN6423@IOT>
2024-09-02 16:52:09 -0300

    implementado like no front

--- test.js
+++ test.js
@@ -1,3 +1,18 @@
-var p = document.createElement("p");
-p.textContent = "Meu texto";
-document.body.appendChild(p);
+var isLiked = false;
+function like() {
+    var button = document.getElementById("btn-like");
+    var icon = button === null || button === void 0 ? void 0 : button.children[0];
+    if (!icon)
+        return;
+    if (isLiked) {
+        icon.classList.remove("fa-heart");
+        icon.classList.remove("liked");
+        icon.classList.add("fa-heart-o");
+    }
+    else {
+        icon.classList.remove("fa-heart-o");
+        icon.classList.add("fa-heart");
+        icon.classList.add("liked");
+    }
+    isLiked = !isLiked;
+}
--- test.ts
+++ test.ts
@@ -1,3 +1,19 @@
-const p = document.createElement("p");
-p.textContent = "Meu texto";
-document.body.appendChild(p);
+let isLiked = false;
+
+function like() {
+  const button = document.getElementById("btn-like");
+  const icon = button?.children[0];
+
+  if (!icon) return;
+  
+  if (isLiked) {
+    icon.classList.remove("fa-heart");
+    icon.classList.remove("liked");
+    icon.classList.add("fa-heart-o");
+  } else {
+    icon.classList.remove("fa-heart-o");
+    icon.classList.add("fa-heart");
+    icon.classList.add("liked");
+  }
+  isLiked = !isLiked;
+}
