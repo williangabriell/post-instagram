6ed5f552c68207fb0a92d3cb622746cb09e4a128
WILLIAN GABRIEL LOPES DA SILVA <WILLIAN6423@IOT>
2024-09-11 15:34:02 -0300

    atualizando likes

--- index.ts
+++ index.ts
@@ -1,16 +1,15 @@
-
 import { v4 as randomUUID } from "uuid";
 import { faker } from "@faker-js/faker";
 
 class Post {
-  private _id: string;
-  private _userName: string; // Atributo privado do tipo string
+  private _id: string = randomUUID();
+  private _userName: string;
   private _avatarUrl: string;
   private _imageUrl: string;
-  private _isLiked: boolean;
+  private _isLiked: boolean = false;
   private _description: string;
-  private _createdAt: Date;
-  private _numberOfLikes: number;
+  private _createdAt: Date = new Date();
+  private _numberOfLikes: number = 0;
 
   constructor(
     userName: string,
@@ -18,87 +17,93 @@ class Post {
     imageUrl: string,
     description: string
   ) {
-    this._id = randomUUID();
-    this._userName = userName;
+    this._userName = userName.toLowerCase();
     this._avatarUrl = avatarUrl;
     this._imageUrl = imageUrl;
-    this._isLiked = false;
     this._description = description;
-    this._createdAt = new Date();
-    this._numberOfLikes = 0;
   }
 
   like() {
+    const postContainer = document.getElementById(this._id);
+    const btnLike = postContainer?.querySelector("#btn-like");
+    const icon = btnLike?.children[0];
+
+    if (!icon) return;
+    this.updateLikeIcon(icon)
+
+
     this._isLiked = !this._isLiked;
-    // Incrementa o número de likes
-    if (this._isLiked) {
-      this._numberOfLikes += 1;
-    } else {
-      this._numberOfLikes -= 1;
-    }
-    const postContainer = document.getElementById(this._id)
-    const btnLike = postContainer?.querySelector('#btn-like')
   }
+
+  private updateLikeIcon(icon: Element) {
+    // 
+    icon.classList.toggle("fa-heart");
+    icon.classList.toggle("liked");
+    icon.classList.toggle("fa-heart-o");
+}
 
   toHTML() {
     const postContainer = document.createElement("div");
     postContainer.className = "post-container";
+    postContainer.id = this._id;
 
     const postHeader = `
-      <div class = 'post-header">
-      <div>
-        <img title = 'Avatar image'
-        src=${this._avatarUrl}
-      </div>
-      <span>${this._userName}</span>
+      <div class="post-header">
+        <div>
+          <img title="Avatar image"
+            src=${this._avatarUrl}>
+        </div>
+        <span>${this._userName}</span>
       </div>
     `;
 
     const postImage = `
-      <div class="post-image"
-        <img title="Post Image"
+     <div class="post-image">
+        <img title="Post image"
           src=${this._imageUrl}>
       </div>
     `;
 
     const postIcons = `
-      <div>
-            <div id="btn-like" onclick="$()">
-              <i class="fa fa-heart-o"></i>
-            </div>
-            <div>
-              <i class="fa fa-comment-o"></i>
-            </div>
-            <div>
-              <i class="fa fa-paper-plane-o"></i>
-            </div>            
+      <div class="post-icons">
+        <div>
+          <div id="btn-like">
+            <i class="fa fa-heart-o"></i>
           </div>
+
           <div>
-            <i class="fa fa-bookmark-o"></i>
+            <i class="fa fa-comment-o"></i>
           </div>
-    `
+
+          <div>
+            <i class="fa fa-paper-plane-o"></i>
+          </div>
+        </div>
+
+        <i class="fa fa-bookmark-o"></i>
+      </div>
+    `;
 
     postContainer.innerHTML = postHeader;
     postContainer.innerHTML += postImage;
     postContainer.innerHTML += postIcons;
 
-    const btnLike = postContainer.querySelector('#btn-like')
-    btnLike?.addEventListener("click", this.like)
+    const btnLike = postContainer.querySelector("#btn-like");
+    btnLike?.addEventListener("click", () => this.like());
 
     document.body.appendChild(postContainer);
-
   }
 }
 
-const posts: Post[] = [];
-
 for (let index = 0; index < 15; index++) {
   const userName = faker.person.firstName();
-  const avatarUrl = faker.image.avatar();
+  const avatarUrl = faker.image.avatarGitHub();
   const imageUrl = faker.image.urlLoremFlickr();
-  const description = faker.lorem.word();
+  const description = faker.lorem.paragraph();
 
   const post = new Post(userName, avatarUrl, imageUrl, description);
 
-  posts.push(post);
+  post.toHTML();
 }
+
+
--- test.ts
+++ test.ts
@@ -5,15 +5,18 @@ function like() {
   const icon = button?.children[0];
 
   if (!icon) return;
-  
+
+  // Remove o coração preenchido e adiciona o coração vazio
   if (isLiked) {
     icon.classList.remove("fa-heart");
     icon.classList.remove("liked");
     icon.classList.add("fa-heart-o");
   } else {
+    // Remove o coração vazio e adiciona o coração preenchido
     icon.classList.remove("fa-heart-o");
     icon.classList.add("fa-heart");
     icon.classList.add("liked");
   }
-  isLiked = !isLiked;
+
+  isLiked = !isLiked; // Inverte o valor de `isLiked`
 }
